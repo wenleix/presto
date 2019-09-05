4c67f1fc7e8364bb1e2e4cd01497ac42ef8cc9c6
Andrii Rosa <andriirosa@fb.com>
2019-09-04 17:29:40 -0700

    Re-enabled some of the checks

--- presto-spark/src/main/java/com/facebook/presto/spark/POSQueryRunner.java
+++ presto-spark/src/main/java/com/facebook/presto/spark/POSQueryRunner.java
@@ -14,27 +14,19 @@
 package com.facebook.presto.spark;
 
 import com.facebook.presto.Session;
-import com.facebook.presto.hive.HiveConnectorFactory;
-import com.facebook.presto.hive.metastore.Database;
-import com.facebook.presto.hive.metastore.ExtendedHiveMetastore;
-import com.facebook.presto.spi.security.PrincipalType;
-import com.facebook.presto.testing.LocalQueryRunner;
 import com.facebook.presto.tpch.TpchConnectorFactory;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.io.Files;
 
 import java.io.File;
-import java.util.Map;
-import java.util.Optional;
 
-import static com.facebook.presto.hive.metastore.file.FileHiveMetastore.createTestingFileHiveMetastore;
 import static com.facebook.presto.testing.TestingSession.testSessionBuilder;
 
 public final class POSQueryRunner
 {
+    private POSQueryRunner() {}
 
     public static POSLocalQueryRunner createLocalQueryRunner()
-
     {
         File tempDir = Files.createTempDir();
 
